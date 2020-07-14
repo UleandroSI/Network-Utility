9da1590b1eed6f6771adb264e5efc1de66446e15
Leandro Barbosa <uleandrosp7@gmail.com>
2020-07-14 18:47:19 -0300

    Versão 1
    Nesta primeira versão existe problema no try. Todas verificações vão para o except.
    
    Arquivo de IPs no padrão de um IP por linha, sem aspas:
    192.168.0.11
    192.168.0.12
    192.168.0.13

---
+++ nslookup-narede.py
@@ -0,0 +1,31 @@
+__author__ = "UleandroSI"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Leandro Barbosa"]
+__license__ = "GNU General Public License v3.0"
+__version__ = "1.0.1"
+__maintainer__ = "Leandro Barbosa"
+__email__ = "uleandrosp7@gmail.com"
+__status__ = "Development"
+
+import socket
+
+# Abrir arquivo .txt que está na mesma pasta
+arquivo = open("IPs.txt", "r")
+
+# Leitura das linhas do arquivo de IPs
+for linha in arquivo:
+    ''' Para utilizar se pretender fazer nslookup pelo nome ao inves do IP
+    addr = socket.gethostbyname('yahoo.com')
+    print(addr)    '''
+    while True:
+    # Verifica se IP está registrado no DNS e trata o erro
+        try:
+            hostName = socket.gethostbyaddr(linha)
+            print(hostName)
+            print("O nome registrado para o IP {} e: {}".format(linha, hostName))
+            break
+        except socket.gaierror:
+            print("srvdc não encontrou ",linha)
+            break
+
+arquivo.close()
